c8b3a194c5163b9519276a103c78fdbeec9b411f
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-03-27 18:58:05 -0500

    design Navbar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,13 @@
 import { Box } from "@mui/material"
+import { Navbar } from "../components"
+
+const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
 
+        <Navbar drawerWidth={ drawerWidth } />
 
         <Box 
             component='main' 
