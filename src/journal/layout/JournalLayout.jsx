20e9f7f39d71d6d90b7e3354d0ee47c4b6a2cf3d
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-03-27 18:37:49 -0500

    created JournalLayout and JournalPage

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+
+
+        <Box 
+            component='main' 
+            sx={{ flexGrow: 1, p: 3 }}    
+        >
+
+            { children }
+        </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography variant="h1">JournalPage</Typography>
-  )
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius perferendis distinctio asperiores ut sapiente reprehenderit debitis voluptatum aut doloremque at perspiciatis ratione minus fugit, facilis accusamus officiis explicabo dicta maiores.
+      </Typography>
+
+    </JournalLayout>
+    )
 }
